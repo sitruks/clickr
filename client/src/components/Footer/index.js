377fbe69fb34883771b92f33edb92921b07863ca
SITRUK <kurtis.santillanes@gmail.com>
2019-07-12 14:28:23 -0700

    adding initial clicky structure

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+
+function Footer() {
+  return (
+    <footer className="footer">
+    <p>Some footer nonsense!</p>
+  </footer>
+  );
+}
+
+export default Footer;
